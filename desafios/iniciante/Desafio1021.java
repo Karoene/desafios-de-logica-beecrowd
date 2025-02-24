0e0a72b612a4561d4faef11a3ed2c4ae6b0dab25
Karoene Mendonca <karoene.d.s.mendonca@gmail.com>
2025-02-24 19:21:22 -0300

    improvement: retaforacao e melhoria do 1021

--- desafios/iniciante/Desafio1021.java
+++ desafios/iniciante/Desafio1021.java
@@ -61,148 +61,34 @@ MOEDAS:
 0 moeda(s) de R$ 0.05
 1 moeda(s) de R$ 0.01
  */
+import java.util.Locale;
+import java.util.Scanner;
 public class Desafio1021{
     public static void main(String[] args) {
-        double money, attmoney;
-        money = 458.01;
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US); // Garante leitura com ponto decimal
+        double money = scanner.nextDouble();
+        scanner.close();        
+        double attmoney = money;  //seria possível tbm *100 para garantir valores de 0.01 e 0.05
 
-        //MEUS DEUS FACA UM LOOP REPETICAO DE CODIGO
-        int quant100 = (int) (money/100);
-        attmoney = money - (quant100*100);
-
-        int quant50 = (int) (attmoney /50);
-        attmoney = attmoney - (quant50*50);
-
-        int quant20 = (int)(attmoney /20);
-        attmoney = attmoney - (quant20*20);
-
-        int quant10 = (int)(attmoney /10);
-        attmoney = attmoney - (quant10*10);
-
-        int quant05 = (int)(attmoney /5);
-        attmoney = attmoney - (quant05*5);
-
-        int quant02 = (int)(attmoney /2);
-        attmoney = attmoney - (quant02*2);
-        //cents
-        int quant01 = (int) (attmoney /1);
-        attmoney = attmoney - (quant01*1);
-
-        int quant005 = (int) (attmoney /0.5);
-        attmoney = attmoney - (quant005*0.5);
-        
-        int quant025 = (int) (attmoney /0.25);
-        attmoney = attmoney - (quant025*0.25);
-
-        int quant001 = (int) (attmoney /0.1);
-        attmoney = attmoney - (quant001*0.1);
-
-        int quant0005 = (int) (attmoney /0.05);
-        attmoney = attmoney - (quant0005*0.05);
-
-        int quant0001 = (int) (attmoney /0.01);
-        attmoney = attmoney - (quant0001*0.01);
+        double[] notas = {100.00, 50.00, 20.00, 10.00, 5.00, 2.00}; 
+        double[] cents = {1.00, 0.50, 0.25, 0.10, 0.05, 0.01};
 
         System.out.println("NOTAS:");
-        System.out.println(quant100 + " nota(s) de R$ 100.00");
-        System.out.println(quant50 + " nota(s) de R$ 50.00");
-        System.out.println(quant20 + " nota(s) de R$ 20.00");
-        System.out.println(quant10 + " nota(s) de R$ 10.00");
-        System.out.println(quant05 + " nota(s) de R$ 5.00");
-        System.out.println(quant02 + " nota(s) de R$ 2.00");
+        for (int i = 0; i < notas.length; i++) {
+            money = attmoney / notas[i];            
+            System.out.println(((int) money) + " nota(s) de R$ " + String.format(Locale.US,"%.2f", notas[i]));            
+            attmoney %= notas[i];
+        }
 
         System.out.println("MOEDAS:");
-        System.out.println(quant01 + " moeda(s) de R$ 1.00");
-        System.out.println(quant005 + " moeda(s) de R$ 0.50");
-        System.out.println(quant025 + " moeda(s) de R$ 0.25");
-        System.out.println(quant001 + " moeda(s) de R$ 0.10");
-        System.out.println(quant0005 + " moeda(s) de R$ 0.05");
-        System.out.println(quant0001 + " moeda(s) de R$ 0.01");
-
-
-       
-
-    }
-}
-
-
-/*
- * 
- * 
- * 
- * import java.util.Locale;
-import java.util.Scanner;
-
-public class teste {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        scanner.useLocale(Locale.US); // Garante que o Scanner use '.' como separador decimal
-
-        if (scanner.hasNextDouble()) { // Evita erro se a entrada não for válida
-            double money = scanner.nextDouble();
-            scanner.close();
-
-            // Multiplica por 100 para evitar problemas de precisão
-            int valor = (int) Math.round(money * 100);
-
-            // Notas
-            int quant100 = valor / 10000;
-            valor %= 10000;
-
-            int quant50 = valor / 5000;
-            valor %= 5000;
-
-            int quant20 = valor / 2000;
-            valor %= 2000;
-
-            int quant10 = valor / 1000;
-            valor %= 1000;
-
-            int quant5 = valor / 500;
-            valor %= 500;
-
-            int quant2 = valor / 200;
-            valor %= 200;
-
-            // Moedas
-            int quant1 = valor / 100;
-            valor %= 100;
-
-            int quant050 = valor / 50;
-            valor %= 50;
-
-            int quant025 = valor / 25;
-            valor %= 25;
-
-            int quant010 = valor / 10;
-            valor %= 10;
-
-            int quant005 = valor / 5;
-            valor %= 5;
-
-            int quant001 = valor; // O restante é a quantidade de moedas de 1 centavo
-
-            // Exibição da saída formatada
-            System.out.println("NOTAS:");
-            System.out.println(quant100 + " nota(s) de R$ 100.00");
-            System.out.println(quant50 + " nota(s) de R$ 50.00");
-            System.out.println(quant20 + " nota(s) de R$ 20.00");
-            System.out.println(quant10 + " nota(s) de R$ 10.00");
-            System.out.println(quant5 + " nota(s) de R$ 5.00");
-            System.out.println(quant2 + " nota(s) de R$ 2.00");
-
-            System.out.println("MOEDAS:");
-            System.out.println(quant1 + " moeda(s) de R$ 1.00");
-            System.out.println(quant050 + " moeda(s) de R$ 0.50");
-            System.out.println(quant025 + " moeda(s) de R$ 0.25");
-            System.out.println(quant010 + " moeda(s) de R$ 0.10");
-            System.out.println(quant005 + " moeda(s) de R$ 0.05");
-            System.out.println(quant001 + " moeda(s) de R$ 0.01");
-        } else {
-            System.out.println("Erro: Entrada inválida!");
+        for (int i = 0; i < cents.length; i++) {
+            money = attmoney / cents[i];
+            System.out.println(((int) money) + " moeda(s) de R$ " + String.format(Locale.US,"%.2f", cents[i]));                          
+            attmoney %= cents[i];
+            attmoney += 0.00001;          
+             //gambiarra assumida para garantir o 0.01
         }
+                
     }
 }
-
-
- */
