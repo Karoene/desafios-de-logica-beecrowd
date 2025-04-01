d15321fc40923bdfdfacf467980f3514be295f32
Karoene Mendonca <karoene.d.s.mendonca@gmail.com>
2025-04-01 12:24:13 -0300

    feat: desafios 19 59 67 e 71

---
+++ desafios/1-iniciante/Desafio1067.java
@@ -0,0 +1,30 @@
+/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha,
+inclusive o X, se for o caso.
+
+Entrada
+O arquivo de entrada contém 1 valor inteiro qualquer.
+8
+
+Saída
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+1
+3
+5
+7
+* */
+
+import java.util.Scanner;
+
+public class Desafio1067 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int x = teclado.nextInt();
+
+        for (int i = 1;i <= x;i++){
+            if (!((i%2)==0)){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
